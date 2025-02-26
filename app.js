588dd46a7e27cdf60787ae2a599d763f7b62c93b
shuaib <mohdshuaib.asahi@gmail.com>
2025-02-26 19:11:16 +0530

    Data Initialized

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userRouter=require("./routes/user.js");
 
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "Views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
--- init/index.js
+++ init/index.js
@@ -1,7 +1,11 @@
+if(process.env.NODE_ENV!="production"){
+    require('dotenv').config({ path: '../.env' });
+}
+
 const mongoose=require("mongoose");
 const Listing=require("../Models/listing.js");
 const dataInit=require("./data.js");
-
+const dbUrl=process.env.ATLASDB_URL;
 
 
 main().then((res)=>{
@@ -12,15 +16,16 @@ main().then((res)=>{
     
 })
 
+
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/hostconnect");
+    await mongoose.connect(dbUrl);
 }
 
 const initDB=async ()=>{
    await Listing.deleteMany({});
    dataInit.data=dataInit.data.map((obj)=>({
     ...obj,
-    owner:"67aed25ad1bdc03d95556441",
+    owner:"67bee93b9c8cdf4470915c09",
    }))
    await Listing.insertMany(dataInit.data);
    console.log("Data was initialized");
